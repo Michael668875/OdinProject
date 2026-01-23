81e19de4880dcb81a62e638202f39ac9d433d3e0
Michael668875 <159018843+Michael668875@users.noreply.github.com>
2026-01-23 18:59:28 +1100

    added sizing

--- etchasketch/etch.js
+++ etchasketch/etch.js
@@ -1,13 +1,37 @@
-function grid() {
-    const container = document.getElementById("grid");
+const container = document.getElementById("grid");
+const button = document.getElementById("squares");
 
-    for (let j = 0; j < 16; j++) {
+button.addEventListener("click", () => {
+    let size = prompt("Enter grid size (4 - 100):");
+
+    size = Number(size);
+
+    if (isNaN(size) || size < 4 || size > 100) {
+        alert("Enter a number between 4 and 100");
+        return;
+    }
+
+    grid(size);
+});
+
+function grid(size) {  
+    // cell size
+    const gridSize = 800
+    const cellSize = gridSize / size;
+
+    // clear old container
+    container.innerHTML="";    
+
+    for (let j = 0; j < size; j++) {
         const row = document.createElement("div");
         row.className = "row";
 
-        for (let i = 0; i < 16; i++) {
+        for (let i = 0; i < size; i++) {
             const cell = document.createElement("div");
             cell.className = "cell";
+
+            cell.style.width = `${cellSize}px`;
+            cell.style.height = `${cellSize}px`;
 
             // add hover behavior
             cell.addEventListener("mouseover", () => {
@@ -21,4 +45,3 @@ function grid() {
     }
 }
 
-grid();
